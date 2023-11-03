ba6e525af37712e39e4b1a366d9d85980b002ab9
Alejandra <otro@gmail.com>
2023-11-03 00:52:53 -0300

    add express.json() to read from req.body

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ require('dotenv').config()
 require("./connection")
 const app = express();
 const port = 3000;
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('¡Trying routes!');
