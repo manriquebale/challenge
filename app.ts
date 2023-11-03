56378b901a7c708a6e6dbe78548a64397fecaa16
Alejandra <otro@gmail.com>
2023-11-03 00:30:11 -0300

    add routes for all the entitys

--- app.ts
+++ app.ts
@@ -8,6 +8,18 @@ app.get('/', (req: Request, res: Response) => {
   res.send('¡Trying routes!');
 });
 
+var moviesRoutes = require('./src/movies/movies.routes');
+app.use('/movies', moviesRoutes);
+
+var directorsRoutes = require('./src/directors/directors.routes');
+app.use('/directors', directorsRoutes);
+
+var actorsRoutes = require('./src/actors/actors.routes');
+app.use('/actors', actorsRoutes);
+
+var showsRoutes = require('./src/shows/shows.routes');
+app.use('/shows', showsRoutes);
+
 app.listen(port, () => {
   console.log(`Application running in http://localhost:${port}`);
 });
